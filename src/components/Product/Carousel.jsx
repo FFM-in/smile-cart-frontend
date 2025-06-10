318107ad5e5dad5ffd53df5a18e5d47486601a3b
ffm-in <arbin.mahapatra@protonmail.com>
2025-06-10 10:30:33 +0530

    Created React query custom hook to fetch product and product list

--- src/components/Product/Carousel.jsx
+++ src/components/Product/Carousel.jsx
@@ -1,75 +1,86 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
+import { useShowProduct } from "hooks/reactQuery/useProductsApi";
 import { Left, Right } from "neetoicons";
 import { Button } from "neetoui";
+import { append } from "ramda";
+import { useParams } from "react-router-dom";
 
-const Carousel = ({ imageUrls, title }) => {
+const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const { slug } = useParams();
+
   const timerRef = useRef(null);
 
-  const handleNext = useCallback(() => {
-    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
-  }, [imageUrls.length]);
+  const { data: { imageUrl, imageUrls: partialImageUrls, title } = {} } =
+    useShowProduct(slug);
 
-  const resetTimer = useCallback(() => {
+  const imageUrls = append(imageUrl, partialImageUrls);
+
+  const handleNext = () =>
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
+
+  const resetTimer = () => {
     clearInterval(timerRef.current);
     timerRef.current = setInterval(handleNext, 3000);
-  }, [handleNext]);
+  };
 
-  const handlePrevious = useCallback(() => {
-    resetTimer();
+  const handlePrevious = () => {
     setCurrentIndex(
       prevIndex => (prevIndex - 1 + imageUrls.length) % imageUrls.length
     );
-  }, [imageUrls.length, resetTimer]);
+    resetTimer();
+  };
 
   useEffect(() => {
     timerRef.current = setInterval(handleNext, 3000);
 
     return () => clearInterval(timerRef.current);
-  }, [handleNext]);
+  }, []);
 
   return (
-    <div className="flex items-center">
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Left}
-        style="text"
-        onClick={handlePrevious}
-      />
-      <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center">
+      <div className="flex items-center">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlePrevious}
+        />
         <img
+          alt={title}
           className="max-w-56 h-56 max-h-56 w-56"
           src={imageUrls[currentIndex]}
         />
-        <div className="flex space-x-1">
-          {imageUrls.map((_, index) => (
-            <span
-              key={index}
-              className={classNames(
-                "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
-                { "neeto-ui-bg-black": index === currentIndex }
-              )}
-              onClick={() => {
-                setCurrentIndex(index);
-                resetTimer();
-              }}
-            />
-          ))}
-        </div>
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
+        />
       </div>
-      <Button
-        alt={title}
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Right}
-        style="text"
-        onClick={() => {
-          handleNext();
-          resetTimer();
-        }}
-      />
+      <div className="flex space-x-1">
+        {imageUrls.map((_, index) => (
+          <span
+            key={index}
+            className={classNames(
+              "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
+              { "neeto-ui-bg-black": index === currentIndex }
+            )}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
+
 export default Carousel;
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,103 +1,21 @@
-import { useState, useEffect } from "react";
-
-import productsApi from "apis/products";
-import {
-  Header,
-  PageLoader,
-  PageNotFound,
-  AddToCart,
-} from "components/commons";
-import useSelectedQuantity from "components/hooks/useSelectedQuantity";
-import { Typography, Button } from "neetoui";
-import { append, isNotNil } from "ramda";
-import { useTranslation } from "react-i18next";
-import { useParams } from "react-router-dom";
+import AddToCart from "components/commons/AddToCart";
+import { Typography } from "neetoui";
+import { Link } from "react-router-dom";
 import routes from "routes";
+import { buildUrl } from "utils/url";
 
-import Carousel from "./Carousel";
+const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
+  <Link
+    className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
+    to={buildUrl(routes.products.show, { slug })}
+  >
+    <img alt={name} className="h-40 w-40" src={imageUrl} />
+    <Typography className="text-center" weight="semibold">
+      {name}
+    </Typography>
+    <Typography>${offerPrice}</Typography>
+    <AddToCart {...{ slug }} />
+  </Link>
+);
 
-const Product = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [product, setProduct] = useState({});
-
-  const { t } = useTranslation();
-
-  const { slug } = useParams();
-
-  const { selectedQuantity, setSelectedQuantity } = useSelectedQuantity(slug);
-
-  const fetchProduct = async () => {
-    try {
-      const response = await productsApi.show(slug);
-      setProduct(response);
-    } catch (error) {
-      setIsError(true);
-      console.log(t("error.genericError", { error }));
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchProduct();
-  }, []);
-
-  const {
-    name,
-    description,
-    mrp,
-    offerPrice,
-    imageUrls,
-    imageUrl,
-    availableQuantity,
-  } = product;
-
-  const totalDiscounts = mrp - offerPrice;
-  const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
-
-  if (isLoading) {
-    return <PageLoader />;
-  }
-
-  if (isError) return <PageNotFound />;
-
-  return (
-    <>
-      <Header title={name} />
-      <div className="mt-16 flex gap-4">
-        <div className="w-2/5">
-          <div className="flex justify-center gap-16">
-            {isNotNil(imageUrls) ? (
-              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
-            ) : (
-              <img alt={name} className="w-48" src={imageUrl} />
-            )}
-          </div>
-        </div>
-        <div className="w-3/5 space-y-4">
-          <Typography>{description}</Typography>
-          <Typography>{t("mrp", { mrp })}</Typography>
-          <Typography className="font-semibold">
-            {t("offerPrice", { offerPrice })}
-          </Typography>
-          <Typography className="font-semibold text-green-600">
-            {t("discountRate", { discountPercentage })}
-          </Typography>
-          <div className="flex space-x-10">
-            <AddToCart {...{ availableQuantity, slug }} />
-            <Button
-              className="bg-neutral-800 hover:bg-neutral-950"
-              label={t("buyNow")}
-              size="large"
-              to={routes.checkout}
-              onClick={() => setSelectedQuantity(selectedQuantity || 1)}
-            />
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Product;
+export default ProductListItem;
