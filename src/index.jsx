b0b1f093e18d0152e416647ad1ca48d038b06b2d
ffm-in <arbin.mahapatra@protonmail.com>
2025-06-10 19:00:17 +0530

    final fixes

--- src/index.jsx
+++ src/index.jsx
@@ -7,13 +7,14 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import queryClient from "utils/queryClient";
 
-import App from "./App";
 import "./common/i18n";
+//i18n should load before App initialization. Hence, disabling import/order rule.
+// eslint-disable-next-line import/order
+import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 initializeAxios();
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
